e8a9a5419cd26cc38a3b5100a27b0099a14f802b
varunmor08 <varun@nextillo.com>
2026-03-16 12:10:02 +0530

    screen shot protect

--- android/app/src/main/java/com/mdhouseapp/MainActivity.kt
+++ android/app/src/main/java/com/mdhouseapp/MainActivity.kt
@@ -4,8 +4,21 @@ import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+import android.os.Bundle
+import android.view.WindowManager
 
 class MainActivity : ReactActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+    // Block screenshots and screen recording
+    window.setFlags(
+      WindowManager.LayoutParams.FLAG_SECURE,
+      WindowManager.LayoutParams.FLAG_SECURE
+    )
+  }
+
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
